cefda4e33b644451c41fba8d6def7af2df272742
Keaton Jones <keatonjones@Keatons-MacBook-Pro.local>
2022-10-05 08:59:22 -0400

    Header Created

---
+++ src/app/header/header.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { HeaderComponent } from './header.component';
+
+@NgModule({
+  declarations: [HeaderComponent],
+  imports: [],
+  providers: [],
+  bootstrap: [HeaderComponent]
+})
+export class HeaderModule {}
